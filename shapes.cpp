c00f3fca126de00a5b50d870f06ed44d942d3533
xiaohanlyu <47364648+xiaohanlyu@users.noreply.github.com>
2019-11-10 17:36:15 +0100

    Add files via upload

---
+++ shapes.cpp
@@ -0,0 +1,59 @@
+#include "shapes.h"
+#include <cmath>
+
+double const pi= 3.14;
+Rectangle::Rectangle(double h, double w){
+    pri_height = h;
+    pri_width = w;
+}
+
+
+double Rectangle::height(){
+    return pri_height;
+}
+
+double Rectangle::width(){
+    return pri_width;
+}
+
+double Rectangle::area(){
+    return pri_width*pri_height;
+}
+
+double Rectangle::perimeter(){
+    return(pri_width+pri_height)*2;
+}
+
+void Rectangle::rotate(){
+    double k;
+    k= pri_width;
+    pri_width= pri_height;
+    pri_height= k;
+}
+Square::Square(double l):Rectangle(l,l){
+    length=l;
+}
+
+Circle::Circle(double r){
+    radius = r;
+}
+
+double Circle::area(){
+    return radius*radius*pi;
+}
+
+double Circle::perimeter(){
+    return 2*pi*radius;
+}
+
+double Circle::height(){
+    return 2*radius;
+}
+
+double Circle::width(){
+    return 2*radius;
+}
+
+void Circle::rotate(){
+    return;
+}
---
+++ shapes.h
@@ -0,0 +1,61 @@
+#ifndef __shapes__
+#define __shapes__
+
+#include <stdio.h>
+
+using namespace std;
+class Shape
+{
+   public:
+
+    virtual double area()=0;
+    virtual double perimeter()=0;
+    virtual double width()=0;
+    virtual double height()=0;
+    virtual void rotate()=0;
+
+};
+
+
+class Rectangle: public Shape
+{
+public:
+    Rectangle(double h, double w);
+    double area();
+    double perimeter();
+    double width();
+    double height();
+    void rotate();
+private:
+    double pri_height;
+    double pri_width;
+
+};
+
+class Square: public Rectangle
+{
+public:
+     Square(double l);
+
+private:
+    double length;
+
+};
+
+
+class Circle: public Shape
+{
+public:
+    Circle(double r);
+    double area();
+    double perimeter();
+    double width();
+    double height();
+    void rotate();
+private:
+    double radius;
+
+};
+
+
+#endif
